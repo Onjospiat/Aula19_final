cb442f8c12682148d5b581d382d5429176079218
Onjospiat <guilherme.chini.gani@gmail.com>
2025-03-23 18:21:07 -0300

    Preparação da aula 19 igual ao final da aula 18

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello!');
+function rolagem(id_secao){
+    const elemento = document.getElementById(id_secao);
+
+    if(elemento){
+        elemento.scrollIntoView({ behavior: 'smooth' });
+}
+
+}
+
+window.rolagem = rolagem;
+
+/*Aula 18*/
+function func_overlay(elemento,i){
+    const text_over = document.getElementsByClassName("overlay-text");
+    text_over[i].style.width = elemento.offsetWidth + "px";
+    text_over[i].style.height = elemento.offsetHeight + "px";
+}
+
+window.func_overlay = func_overlay;
